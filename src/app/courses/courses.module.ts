7c1730103861d7113970a2a10fc3b57e034bacd8
advalker <advalker@hotmail.com>
2024-04-16 00:00:19 -0300

    atualização do corses. html

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
+import { MatTableModule } from '@angular/material/table';
+
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    CoursesRoutingModule
+    CoursesRoutingModule,
+    MatTableModule,
+    MatCardModule
   ]
 })
 export class CoursesModule { }
--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-
+import { MatTableModule } from '@angular/material/table';
+import { Course } from '../model/course';
+import { MatCardModule } from '@angular/material/card';
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatCardModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
+
+  courses: Course[] = [
+    {
+      _id: '1', name: 'Angula', category: 'frontend'
+    },
+    {
+      _id: '2', name: 'Java', category: 'backend'
+    }
+  ];
+  displayedColumns = ['name', 'category'];
+
+  constructor() {
+    //this.courses = []
+  }
 
 }
